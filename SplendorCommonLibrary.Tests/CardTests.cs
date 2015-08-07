375b4a7d4afe8c0768ec5411f1d1567b52937b4a
Marcin Ceglarek <marcin.ceglarek@gmail.com>
2015-08-07 21:04:30 +0200

    Updated card's unit tests

--- SplendorCommonLibrary.Tests/CardTests.cs
+++ SplendorCommonLibrary.Tests/CardTests.cs
@@ -10,53 +10,65 @@
         [TestMethod]
         public void Card_CanBuy_NormalTest()
         {
-            var card = new Card() { Color = Color.White, Cost = new Chips() { Black = 2, Blue = 2, Red = 2, White = 2 } };
+            var card = new Card() { Color = Color.White, Cost = new Chips() { Black = 2, Blue = 2, Green = 2, Red = 2, White = 2 } };
 
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 5, Red = 5, White = 5}));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 2, Blue = 5, Red = 5, White = 5 }));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 2, Red = 2, White = 5 }));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 5, Red = 5, White = 2 }));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 2, Blue = 2, Red = 2, White = 2 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 5, Green = 5, Red = 5, White = 5}));
+
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 2, Blue = 5, Green = 5, Red = 5, White = 5 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 2, Green = 5, Red = 5, White = 5 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 5, Green = 2, Red = 5, White = 5 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 5, Green = 5, Red = 2, White = 5 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 5, Green = 5, Red = 5, White = 2 }));
+            
         }
 
         [TestMethod]
         public void Card_CanBuy_UseGoldTest()
         {
-            var card = new Card() { Color = Color.White, Cost = new Chips() { Black = 2, Blue = 2, Red = 2, White = 2 } };
+            var card = new Card() { Color = Color.White, Cost = new Chips() { Black = 2, Blue = 2, Green = 2, Red = 2, White = 2 } };
 
-            Assert.IsTrue(card.CanBuy(new Chips() { Blue = 5, Red = 5, White = 5, Gold = 2}));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 2, Red = 5, White = 5, Gold = 2 }));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 2, White = 5, Gold = 2 }));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 5, Red = 5, Gold = 2 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 0, Blue = 5, Green = 5, Red = 5, White = 5, Gold = 2}));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 2, Blue = 0, Green = 5, Red = 5, White = 5, Gold = 2 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 2, Green = 0, Red = 5, White = 5, Gold = 2 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 2, Green = 5, Red = 0, White = 5, Gold = 2 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 5, Blue = 5, Green = 5, Red = 5, White = 0, Gold = 2 }));
 
-            Assert.IsTrue(card.CanBuy(new Chips() { Blue = 1, Red = 5, Gold = 5 }));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 1, Red = 5, Gold = 5 }));
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 1, Blue = 5, Gold = 5 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 0, Blue = 1, Green = 2, Red = 5, Gold = 5 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 1, Blue = 1, Green = 5, Red = 2, Gold = 5 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 1, Blue = 2, Green = 0, Red = 0, White = 5, Gold = 5 }));
 
-            Assert.IsTrue(card.CanBuy(new Chips() { Black = 1, Blue = 1, Gold = 6 }));
-            Assert.IsTrue(card.CanBuy(new Chips() { Gold = 8 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Black = 1, Blue = 1, Gold = 8 }));
+            Assert.IsTrue(card.CanBuy(new Chips() { Gold = 10 }));
         }
 
         [TestMethod]
         public void Card_CanBuy_NegativeTest()
         {
-            var card = new Card() { Color = Color.White, Cost = new Chips() { Black = 2, Blue = 2, Red = 2, White = 2 } };
+            var card = new Card() { Color = Color.White, Cost = new Chips() { Black = 2, Blue = 2, Green = 2, Red = 2, White = 2 } };
 
             Assert.IsFalse(card.CanBuy(new Chips()));
             Assert.IsFalse(card.CanBuy(new Chips() { Black = 1, Blue = 1, Red = 1, White = 1 }));
-            
-            Assert.IsFalse(card.CanBuy(new Chips() { Black = 1, Blue = 2, Red = 2, White = 2 }));
-            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 1, Red = 2, White = 2 }));
-            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Red = 1, White = 2 }));
-            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Red = 2, White = 1 }));
 
-            Assert.IsFalse(card.CanBuy(new Chips() { Blue = 2, Red = 2, White = 2, Gold = 1 }));
-            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Red = 2, White = 2, Gold = 1 }));
-            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, White = 2, Gold = 1 }));
-            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Red = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 0, Blue = 2, Green = 2, Red = 2, White = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 0, Green = 2, Red = 2, White = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Green = 0, Red = 2, White = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Green = 2, Red = 0, White = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Green = 2, Red = 2, White = 0, Gold = 1 }));
 
-            Assert.IsFalse(card.CanBuy(new Chips() { Black = 1, Blue = 1, Gold = 5 }));
-            Assert.IsFalse(card.CanBuy(new Chips() { Gold = 7 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 0, Blue = 1, Green = 1, Red = 1, White = 1, Gold = 4 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 1, Blue = 0, Green = 1, Red = 1, White = 1, Gold = 4 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 1, Blue = 1, Green = 0, Red = 1, White = 1, Gold = 4 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 1, Blue = 1, Green = 1, Red = 0, White = 1, Gold = 4 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 1, Blue = 1, Green = 1, Red = 1, White = 0, Gold = 4 }));
+
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 0, Blue = 2, Green = 2, Red = 2, White = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 0, Green = 2, Red = 2, White = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Green = 0, Red = 2, White = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Green = 2, Red = 0, White = 2, Gold = 1 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 2, Blue = 2, Green = 2, Red = 2, White = 0, Gold = 1 }));
+
+            Assert.IsFalse(card.CanBuy(new Chips() { Black = 1, Blue = 1, Gold = 7 }));
+            Assert.IsFalse(card.CanBuy(new Chips() { Gold = 9 }));
         }
     }
 }
