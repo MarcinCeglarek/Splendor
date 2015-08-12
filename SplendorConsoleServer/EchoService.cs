66bbf53e8de7d429b7cddada40c91973e7988259
Marcin Ceglarek <marcin.ceglarek@gmail.com>
2015-08-12 22:12:13 +0200

    Removed old MVC application, added simple echo web-socket solution

---
+++ SplendorConsoleServer/EchoService.cs
@@ -0,0 +1,43 @@
+﻿namespace SplendorConsoleServer
+{
+    using System;
+
+    using Microsoft.ServiceModel.WebSockets;
+
+    public class EchoService : WebSocketService
+    {
+        #region Public Methods and Operators
+
+        public override void OnMessage(string message)
+        {
+            Console.WriteLine("Echoing to client:");
+            Console.WriteLine(message);
+
+            this.Send(message);
+        }
+
+        public override void OnOpen()
+        {
+            base.OnOpen();
+            Console.WriteLine("WebSocket opened.");
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnClose()
+        {
+            base.OnClose();
+            Console.WriteLine("WebSocket closed.");
+        }
+
+        protected override void OnError()
+        {
+            base.OnError();
+            Console.WriteLine("WebSocket error occured.");
+        }
+
+        #endregion
+    }
+}
---
+++ SplendorConsoleServer/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SplendorConsoleServer
+{
+    #region
+
+    using System;
+
+    using Microsoft.ServiceModel.WebSockets;
+
+    #endregion
+
+    public class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            var host = new WebSocketHost<EchoService>(new Uri("ws://localhost:8000/echo"));
+            host.AddWebSocketEndpoint();
+
+            /*
+             * To properly run next line:
+             * either it must be run as administrator (simpler for dev)
+             * or url needs to be allowed to create by current user in system by command:
+             * netsh http add urlacl url=http://+:8000/echo user=<username>
+             */
+            host.Open();
+
+            Console.Read();
+
+            host.Close();
+        }
+
+        #endregion
+    }
+}
